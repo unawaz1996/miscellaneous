41e0dde55d35fa85af8255c1fcc7ff9862cf33db
unawaz1996 <a1654797@adelaide.edu.au>
2019-08-07 11:34:26 +0930

    changes

--- SpliceAI/split_vcf.sh
+++ SpliceAI/split_vcf.sh
@@ -22,23 +22,6 @@ echo "Moving all output directories to $OUTDIR"
 mv *_dir $OUTDIR
 ### change this script so that a separate folder is created for each sample 
 
-
-#(Another slurm job?)
-### split by chromosome 
-## Get chromosomes that are present in the VCF:
-#cat $1 | mawk '$1 ~ /^#/ {next} {print $1 | "sort -k1,1 -u"}' > ${1%.vcf}_chrs.txt
-
-## Compress & index file with tabix:
-#cat $1 | mawk '$1 ~ /^#/ {print $0;next} {print $0 | "sort -k1,1 -k2,2n"}' | bgzip -c > ${1%.vcf}_sorted.vcf.gz
-#tabix -p vcf ${1%.vcf}_sorted.vcf.gz
-
-## Extract files by chr:
-#cat ${1%.vcf}_chrs.txt | parallel "tabix -h ${1%.vcf}_sorted.vcf.gz {} > ${1%.vcf}_{}.vcf"
-
-
-
-
-
 ### attempting to split a VCF
 # maybe by samples instead of chromosomes 
 ### command that Mark has provided me 
